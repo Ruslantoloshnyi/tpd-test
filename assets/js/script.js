aae94ec9412b7fa47f4acad4c03fc9ef84d18ef9
Ruslan <ru@skylinehealthgroup.com>
2023-10-22 11:21:37 +0300

    add feautured products section html and css

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+"use strict"
+
+// $(function () {
+//     $(".carousel").slick({
+//         dots: true,
+//         infinite: true,
+//         speed: 500,
+//         fade: true,
+//         cssEase: 'linear',
+//         prevArrow: '<button type="button" class="slick-prev">&#60;</button>',
+//         nextArrow: '<button type="button" class="slick-next">&#62;</button>',
+//     });
+// });
+
+// $(document).ready(function () {
+//     $('.multiple-items').slick({
+//         infinite: true,
+//         slidesToShow: 3,
+//         slidesToScroll: 3
+//     });
+// });
+
+$(document).ready(function(){
+    $('.carousel').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+         prevArrow: '<button type="button" class="slick-prev">&#10094;</button>',
+         nextArrow: '<button type="button" class="slick-next">&#10095;</button>',
+    });
+});
